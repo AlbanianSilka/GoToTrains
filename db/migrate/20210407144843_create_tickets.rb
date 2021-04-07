1bd25331204106214c0c49b64f935b917976846e
AlbanianSilka <andrii.khavriuta@gmail.com>
2021-04-07 18:40:08 +0300

    Bug fixing and visual changes

--- db/migrate/20210407144843_create_tickets.rb
+++ db/migrate/20210407144843_create_tickets.rb
@@ -5,7 +5,7 @@ class CreateTickets < ActiveRecord::Migration[6.1]
   def change
     create_table :tickets do |t|
       t.integer :user_id
-      t.integer :train_number
+      t.integer :train_id
       t.integer :first_station_id
       t.integer :last_station_id
       t.timestamps
