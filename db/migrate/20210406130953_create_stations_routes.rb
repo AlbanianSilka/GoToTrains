1cf21c74b53cc6b9415a34cbf8b1426b3b06d41a
AlbanianSilka <andrii.khavriuta@gmail.com>
2021-04-07 14:21:42 +0300

    Updated station routes rb.

--- db/migrate/20210406130953_create_stations_routes.rb
+++ db/migrate/20210406130953_create_stations_routes.rb
@@ -4,8 +4,8 @@
 class CreateStationsRoutes < ActiveRecord::Migration[6.1]
   def change
     create_table :railway_stations_routes do |t|
-      t.integer :railway_station_id
-      t.integer :route_id
+      t.belongs_to :railway_station, index: true
+      t.belongs_to :route, index: true
     end
   end
 end
