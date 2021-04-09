8a9ff4686d26a4f0cf5b3b2993a5df7ec4f320aa
AlbanianSilka <andrii.khavriuta@gmail.com>
2021-04-09 14:36:56 +0300

    Edited trains page a bit to show cars and seats on it.

--- app/models/train.rb
+++ app/models/train.rb
@@ -3,6 +3,7 @@
 # Model for a train and his validation
 class Train < ActiveRecord::Base
   validates :number, presence: true
+  has_many :cars, foreign_key: :current_train_id
   has_one :trains_route
   has_one :route, through: :trains_route
   belongs_to :current_station, class_name: 'RailwayStation', foreign_key: :current_station_id, optional: true
