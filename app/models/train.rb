09a77cbc5c7623f565170537f71e938426971a74
AlbanianSilka <andrii.khavriuta@gmail.com>
2021-04-13 18:02:30 +0300

    Added flag for a trains, now it can count cars from the "head" or from the "tail".

--- app/models/train.rb
+++ app/models/train.rb
@@ -3,8 +3,12 @@
 # Model for a train and his validation
 class Train < ActiveRecord::Base
   validates :number, presence: true, uniqueness: true
-  has_many :cars, foreign_key: :current_train_id
+  has_many :cars
   has_one :trains_route
   has_one :route, through: :trains_route
   belongs_to :current_station, class_name: 'RailwayStation', foreign_key: :current_station_id, optional: true
+
+  has_many :cars, ->(train) { order("number #{train.direction ? 'ASC' : 'DESC'}") }
+
+  scope :get_number_of_seats, ->(car_type, seats_type) { cars.where(type: car_type).sum(seats_type) }
 end
