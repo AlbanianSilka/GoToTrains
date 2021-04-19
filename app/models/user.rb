b9dea9751b388f280fab462772a73f30eef30fe0
AlbanianSilka <andrii.khavriuta@gmail.com>
2021-04-19 11:56:29 +0300

    Created new User model with 'devise'

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,9 @@
 
 # Model for a users
 class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable, :confirmable
   has_many :tickets
 end
