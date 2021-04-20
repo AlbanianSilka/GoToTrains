3e6714c4dfa031871e4619711230fb7b19a1abac
AlbanianSilka <andrii.khavriuta@gmail.com>
2021-04-20 11:18:03 +0300

    Moved files to new admin direction

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,23 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :trains
-  resources :railway_stations do
-    member do
-      post :update_position
-      post :update_time
-    end
-  end
-  resource :search, only: %i[show new edit] do
-    post '/', to: 'searches#search'
-  end
-  resources :routes
+  resource :search, only: [:new, :show, :edit, :create]
+  # resource :search, only: %i[show new edit] do
+  #   post '/', to: 'searches#search'
+  # end
   resources :tickets
-  resources :cars
+  namespace :admin do
+    resources :railway_stations do
+      member do
+        post :update_position
+        post :update_time
+      end
+    end
+    resources :routes
+    resources :trains do
+      resources :cars, shallow: true
+    end
+    resources :tickets
+  end
   get 'welcome/index'
   root 'welcome#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
