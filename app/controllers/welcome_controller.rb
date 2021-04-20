31b9410c6905bf768ad991be9bc413c957ac9b64
AlbanianSilka <andrii.khavriuta@gmail.com>
2021-04-20 18:13:34 +0300

    Updated welcome controller for admin

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,9 @@
 class WelcomeController < ApplicationController
+  before_action :authenticate_user!
+  before_action :check_admin
+  def check_admin
+    redirect_to tickets_path, alert: 'У вас нет прав на просмотр этой страницы' unless current_user.admin?
+  end
   def index
   end
 end
