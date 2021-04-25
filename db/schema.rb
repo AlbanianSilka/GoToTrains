bbcd8a0482681e337cab6c0b0a5254b40984ca61
AlbanianSilka <andrii.khavriuta@gmail.com>
2021-04-25 13:28:30 +0300

    Updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_19_102321) do
+ActiveRecord::Schema.define(version: 2021_04_23_112219) do
 
   create_table "cars", force: :cascade do |t|
     t.integer "number"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2021_04_19_102321) do
     t.integer "side_seats_top"
     t.integer "side_seats_bottom"
     t.integer "sitting_seats"
+    t.index ["id", "type"], name: "index_cars_on_id_and_type"
     t.index ["train_id"], name: "index_cars_on_train_id"
   end
 
@@ -57,7 +58,10 @@ ActiveRecord::Schema.define(version: 2021_04_19_102321) do
     t.datetime "updated_at", precision: 6, null: false
     t.text "user_first_name"
     t.text "user_last_name"
-    t.text "user_passport"
+    t.integer "user_passport"
+    t.index ["first_station_id"], name: "index_tickets_on_first_station_id"
+    t.index ["last_station_id"], name: "index_tickets_on_last_station_id"
+    t.index ["user_id"], name: "index_tickets_on_user_id"
   end
 
   create_table "trains", force: :cascade do |t|
@@ -67,6 +71,7 @@ ActiveRecord::Schema.define(version: 2021_04_19_102321) do
     t.integer "route_id"
     t.integer "current_station_id"
     t.boolean "direction"
+    t.index ["current_station_id"], name: "index_trains_on_current_station_id"
     t.index ["route_id"], name: "index_trains_on_route_id"
   end
 
